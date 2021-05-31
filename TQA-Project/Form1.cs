cf21b65e0c1cf5f091bf7223c56d869ca8ba4f6b
Maryam-21 <58235353+Maryam-21@users.noreply.github.com>
2021-06-01 01:53:01 +0200

    Update Form1.cs
    Fixed bug in input script.
    Added initial code for reduction.

--- TQA-Project/Form1.cs
+++ TQA-Project/Form1.cs
@@ -18,6 +18,7 @@ namespace TQA_Project
         public static List<List<string>> valuesOfConditions;
         public static List<List<string>> valuesOfActions;
         public static List<Tuple<List<string>,List<string>>> script;
+        public static List<Tuple<List<string>, List<string>>> decisionTable;
 
 
 
@@ -29,7 +30,7 @@ namespace TQA_Project
             valuesOfActions = new List<List<string>>();
             valuesOfConditions = new List<List<string>>();
             script = new List<Tuple<List<string>, List<string>>>();
-
+            decisionTable = new List<Tuple<List<string>, List<string>>>();
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -64,7 +65,6 @@ namespace TQA_Project
             valuesOfActions.Add(new List<string>());
             foreach (string v in values)
             {
-
                 valuesOfActions[c].Add(v);
             }
             action_txt.Clear();
@@ -74,18 +74,50 @@ namespace TQA_Project
         private void submit_btn_Click(object sender, EventArgs e)
         {
             string[] values = test_scenario_txt.Text.Split('\n');
-            script.Add(new Tuple<List<string>, List<string>>(new List<string>(), new List<string>()));
+            //script.Add(new Tuple<List<string>, List<string>>(new List<string>(), new List<string>()));
             int c = 0;
             foreach (string v in values)
             {
                 string[] cond = v.Split(',');
-                for(int i=0; i<cond.Length-1; i++)
+                script.Add(new Tuple<List<string>, List<string>>(new List<string>(), new List<string>()));
+
+                for (int i=0; i<cond.Length-1; i++)
                 {
                     script[c].Item1.Add(cond[i]);
                 }
                 script[c].Item2.Add(cond[cond.Length - 1]);
+                c++;
             }
-            c++;
+           
+
+
+            //reduction
+            //the idea is to find 2 cols with the same values for all conditions except for one
+            //replace the one with - 
+            for(int i= 0; i< script.Count; i++)
+            {
+                for(int j=1; j< script.Count; j++)
+                {
+                    if (script[i].Item2[0] == script[j].Item2[0])
+                    {
+                        int count = 0; 
+                        int index=-1;
+                        for(int k=0; k< conditions.Count; k++)
+                        {
+                            if (script[i].Item1[k] != script[j].Item1[k])
+                            {
+                                count++;
+                                index = k;
+                            }
+                        }
+                        if (count == 1)
+                        {
+                            script.RemoveAt(j);
+                            script[i].Item1[index] = "-";
+                        }
+                    }
+                }
+            }
         }
     }
 }
